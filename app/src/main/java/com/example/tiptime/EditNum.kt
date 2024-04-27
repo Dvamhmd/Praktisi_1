2471fda6e8c787d449a3fca01a2fe2996f241908
Dvamhmd <davamohamadrizky@gmail.com>
2024-04-27 19:22:35 +0700

    Tugas_1_Praktisi

---
+++ app/src/main/java/com/example/tiptime/EditNum.kt
@@ -0,0 +1,10 @@
+package com.example.tiptime
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+
